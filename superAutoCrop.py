b3b65189d92c4ba9f23e157d0a9a6a194e14672c
Luciano Cequinel <lucianocequinel@gmail.com>
2021-12-18 04:48:39 -0300

    Update superAutoCrop.py

--- superAutoCrop.py
+++ superAutoCrop.py
@@ -1,18 +1,12 @@
-##############################################################################
-#                                                                            #
-# superAutoCrop                                                              #
-# V 1.6                                                                      #
-# Release January 09 2021                                                    #
-#                                                                            #
-# Did as a thankful gift to my mentor and friend Emerson Bonadias.           #
-#                                                                            #
-# Created by Luciano Cequinel (vimeo.com/cequinavfx)                         #
-# to report bugs or suggestions lucianocequinel@gmail.com                    #
-#                                                                            #
-##############################################################################
-
-
 '''
+ superAutoCrop
+ V 2.0.0
+ Release December 19 2021
+
+ Did as a thankful gift to my mentor and friend Emerson Bonadias.
+
+ Created by Luciano Cequinel (vimeo.com/cequinavfx)
+ to report bugs or suggestions lucianocequinel@gmail.com
 
 Just write
 import superAutoCrop
@@ -27,118 +21,205 @@ See the ref_menu.py as a reference
 import nuke
 
 
+def run_AutoCrop(selNode):
+
+    z = nuke.Panel('superAutoCrop')
+
+    range = nuke.FrameRange('%s-%s' % (nuke.root().firstFrame(), nuke.root().lastFrame()))
+
+    z.addSingleLineInput('frame range', range)
+
+    z.addBooleanCheckBox('use rgb', False)
+
+    z.setWidth(300)
+    result = z.show()
+
+    if result == True:
+
+        print ()
+
+        a, b = z.value('frame range').split('-')
+
+        frame_range = nuke.FrameRange('%s-%s' % (a, b))
+        useRGB = z.value('use rgb')
+
+        selNode = nuke.selectedNode()
+        nkRoot = nuke.root()
+
+        # get output from selected node
+        output = nuke.dependentNodes(nuke.INPUTS, selNode)
+
+
+        # Get Width & Height
+        wNode = selNode.width()
+        hNode = selNode.height()
+
+
+        # Create a CurveTool
+
+        print ('creating CurveTool...')
+
+        cTool = nuke.nodes.CurveTool()
+        cTool.setInput(0, selNode)
+        cTool['operation'].setValue('Auto Crop')
+
+        if useRGB == True:
+            cTool['channels'].setValue('rgba')
+            print ('...using rgb...')
+        else:
+            cTool['channels'].setValue('alpha')
+            print ('...using alpha only...')
+        
+        cTool['resetROI'].setValue('True')
+        cTool.knob("ROI").setValue( [ 0, 0 , selNode.width() , selNode.height() ] )
+        cTool.setInput(0, selNode)
+
+
+        #Execute CurveTool
+        print ('executing AutoCrop...')
+        nuke.execute(cTool, frame_range.first(), frame_range.last())
+
+
+        # create a group
+
+        print ('creating Nodes...')
+
+        grAutoCrop = nuke.createNode('Group')
+
+
+        grAutoCrop.setName('superAutoCrop_%s_' % (selNode.name()), uncollide=True)
+        grAutoCrop.knob('tile_color').setValue(31552)
+        grAutoCrop.knob('note_font').setValue('Verdana bold')
+
+        grAutoCrop.begin()
+
+        # create an Input node
+        grInput = nuke.createNode('Input')
+
+
+        # Create New Crop with data from CurveTool
+        nCrop = nuke.createNode('Crop')
+        nCrop.knob("box").copyAnimations(cTool.knob("autocropdata").animations())
+
+        nCrop.setInput(0, grInput)
+
+        nCrop.knob('tile_color').setValue(31552) #01050
+        nCrop.knob('note_font').setValue('Verdana bold')
+
+        nuke.autoplace(nCrop)
+
+        # create an Output node
+        grOutput = nuke.createNode('Output')
+        grOutput.setInput(0, nCrop)
+
+        grOutput.hideControlPanel()
+
+        nuke.autoplace(grOutput)
+
+        grAutoCrop.end()
+
+
+        grAutoCrop.setInput(0, selNode)
+
+        output = nuke.toNode(output[0].name())
+        output.setInput(0, grAutoCrop)
+
+        nuke.autoplace(grAutoCrop)
+
+        # Create knobs to control the bounding box
+        tab = nuke.Tab_Knob('Size Control')
+        grAutoCrop.addKnob(tab)
+        
+        gS = nuke.Double_Knob('genSize',"Proportional Size")
+        gS.setRange(0,300)
+        grAutoCrop.addKnob(gS)
+
+        gSoft = nuke.Double_Knob('genSoftness',"Softness")
+        gSoft.setRange(0,100)
+        grAutoCrop.addKnob(gSoft)
+
+        div = nuke.Text_Knob("divider","")
+        grAutoCrop.addKnob(div)
+
+        lS = nuke.Double_Knob('lSize',"Left")
+        lS.setRange(-50,50)
+        grAutoCrop.addKnob(lS)
+
+        rS = nuke.Double_Knob('rSize',"Right")
+        rS.setRange(-50,50)
+        grAutoCrop.addKnob(rS)
+
+        tS = nuke.Double_Knob('tSize',"Top")
+        tS.setRange(-50,50)
+        grAutoCrop.addKnob(tS)
+        
+        bS = nuke.Double_Knob('bSize',"Bottom")
+        bS.setRange(-50,50)
+        grAutoCrop.addKnob(bS)
+
+        c = nuke.Text_Knob('c0', '')
+        grAutoCrop.addKnob(c)
+
+        c = nuke.Text_Knob('c1', '', '<font color = "#EF4E3D">Version 2.0.0')
+        grAutoCrop.addKnob(c)
+
+        c = nuke.Text_Knob('c2', '', '<font color = "#EF4E3D">Created by Luciano Cequinel')
+        grAutoCrop.addKnob(c)
+
+        c = nuke.Text_Knob('c3', '', '<font color = "#EF4E3D">Check for updates <a href=\"https://www.cequinavfx.com"><font color=#EF4E3D><b>here</a>')
+        grAutoCrop.addKnob(c)
+
+
+        # Set expressions to knobs
+
+        grAutoCrop['genSize'].setValue(50)
+
+        grName = grAutoCrop.name()
+
+        nCrop.knob('box').setExpression('(curve) + ((%s.knob.lSize) + (%s.knob.genSize) *-1)' %(grName, grName), 0 )
+        nCrop.knob('box').setExpression('(curve) + ((%s.knob.bSize) + (%s.knob.genSize) *-1)' %(grName, grName), 1)
+        nCrop.knob('box').setExpression('(curve) + (%s.knob.rSize) + (%s.knob.genSize)' %(grName, grName), 2)
+        nCrop.knob('box').setExpression('(curve) + (%s.knob.tSize) + (%s.knob.genSize)' %(grName, grName), 3)
+
+        nCrop.knob('softness').setExpression('(%s.knob.genSoftness)' %(grName))
+
+        # Delete CurveTool
+        nuke.delete(cTool)
+
+
+
 def superAutoCrop():
 
-    # Try get the selected node
-    try:
-        selNode = nuke.selectedNodes()
+    # get the selected node
+    selNode = nuke.selectedNodes()
 
-        if len(selNode) < 1:
-            nuke.message('Select any node!')
-            return
-
-        if len(selNode) > 1:
-           nuke.message('Select just one node!')
-           return
-
-    except:
+    if len(selNode) < 1:
         nuke.message('Select any node!')
         return
 
-    # Try to get the frame range
-    try:
-        frame_range = nuke.FrameRange(nuke.getInput('Frame Range', '%s-%s' % (nuke.root().firstFrame(), nuke.root().lastFrame())))
-    except:
+    elif len(selNode) > 1:
+       nuke.message('Select just one node!')
+       return
+
+    elif len(selNode) == 0:
+        nuke.message('Select any node!')
         return
 
-    selNode = nuke.selectedNode()
-    nkRoot = nuke.root()
+    else:
+        selNode = nuke.selectedNode()
 
-
-    # Get Width & Height
-    wNode = selNode.width()
-    hNode = selNode.height()
-
-
-    # Create a CurveTool
-    cTool = nuke.nodes.CurveTool()
-    cTool.setInput(0, selNode)
-    cTool['operation'].setValue('Auto Crop')
-    cTool['channels'].setValue('alpha')
-    cTool['resetROI'].setValue('True')
-    cTool.knob("ROI").setValue( [ 0, 0 , selNode.width() , selNode.height() ] )
-    cTool.setInput(0, selNode)
-
-
-    #Execute CurveTool
-    nuke.execute(cTool, frame_range.first(), frame_range.last())
-
-
-    # Create New Crop with data from CurveTool
-    nCrop = nuke.nodes.Crop()
-    nCrop.knob("box").copyAnimations(cTool.knob("autocropdata").animations())
-    nCrop['label'].setValue('AutoCrop from %s' %(selNode.name()))
-    nCrop.setInput(0, selNode)
-    nCrop.knob('tile_color').setValue(01050)
-    nCrop.knob('note_font').setValue('Verdana bold')
-
-
-    # Create new Tab and Knobs to control the bounding box
-    tab = nuke.Tab_Knob('Size Control')
-    nCrop.addKnob(tab)
-    
-    gS = nuke.Double_Knob('genSize',"Proportional Size")
-    gS.setRange(0,300)
-    nCrop.addKnob(gS)
-
-    div = nuke.Text_Knob("divider","")
-    nCrop.addKnob(div)
-
-    lS = nuke.Double_Knob('lSize',"Left")
-    lS.setRange(-50,50)
-    nCrop.addKnob(lS)
-
-    rS = nuke.Double_Knob('rSize',"Right")
-    rS.setRange(-50,50)
-    nCrop.addKnob(rS)
-
-    tS = nuke.Double_Knob('tSize',"Top")
-    tS.setRange(-50,50)
-    nCrop.addKnob(tS)
-    
-    bS = nuke.Double_Knob('bSize',"Bottom")
-    bS.setRange(-50,50)
-    nCrop.addKnob(bS)
-
-    c = nuke.Text_Knob('c0', '')
-    nCrop.addKnob(c)
-
-    c = nuke.Text_Knob('c1', '', '<font color = "#EF4E3D">Version 1.6')
-    nCrop.addKnob(c)
-
-    c = nuke.Text_Knob('c2', '', '<font color = "#EF4E3D">Created by Luciano Cequinel')
-    nCrop.addKnob(c)
-
-    c = nuke.Text_Knob('c3', '', '<font color = "#EF4E3D">Check for updates <a href=\"https://www.cequinavfx.com"><font color=#EF4E3D><b>here</a>')
-    nCrop.addKnob(c)
-
-
-    # Set expressions to knobs
-
-    nCrop['genSize'].setValue(30)
-
-    nCrop.knob('box').setExpression('(curve) + ((knob.lSize) + (knob.genSize) *-1)', 0)
-    nCrop.knob('box').setExpression('(curve) + ((knob.bSize) + (knob.genSize) *-1)', 1)
-    nCrop.knob('box').setExpression('(curve) + (knob.rSize) + (knob.genSize)', 2)
-    nCrop.knob('box').setExpression('(curve) + (knob.tSize) + (knob.genSize)', 3)
-
-
-    # Delete CurveTool
-    nuke.delete(cTool)
+        if selNode.Class() == 'Viewer':
+            nuke.message("Select node can't be a Viewer")
+            return
+        else:
+            run_AutoCrop(selNode)
+            print ('AutoCrop successfully created')
+            return
 
 
 
+superAutoCrop()
 #Add a menu and assign a shortcut
-toolbar = nuke.menu('Nodes')
-cqnTools = toolbar.addMenu('CQNTools', icon='Modify.png')
-cqnTools.addCommand('superAutoCrop', 'superAutoCrop.superAutoCrop()', '[', icon='AutoCrop.png')
+#toolbar = nuke.menu('Nodes')
+#cqnTools = toolbar.addMenu('CQNTools', icon='Modify.png')
+#cqnTools.addCommand('superAutoCrop', 'superAutoCrop.superAutoCrop()', '[', icon='AutoCrop.png')
